e3be7bc2a74894ab0dfc9e4cab7f1e868d7b258a
Abhinay Pandey <abhinaypandey02@gmail.com>
2026-03-22 10:14:05 +0000

    options

--- app/api/(graphql)/route.ts
+++ app/api/(graphql)/route.ts
@@ -2,7 +2,7 @@ import {setupGraphQL} from 'naystack/graphql'
 import OrderResolvers, {OrderFields} from './order'
 import UserResolvers from './user'
 
-export const {GET, POST} = await setupGraphQL({
+export const {GET, POST, OPTIONS} = await setupGraphQL({
   resolvers:[UserResolvers, OrderResolvers, OrderFields],
   allowedOrigins: ['https://1soga.com', 'https://onesoga.com']
 })
