f1b3eb39d7f23bdffe4d08c6eb2ef15ecf2397d6
Abhinay Pandey <abhinaypandey@uber.com>
2026-05-16 13:45:00 +0530

    refund

--- app/page.tsx
+++ app/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
           </span>
           </div>
 
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-5 lg:grid-cols-3">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-5 lg:grid-cols-4">
             {products.map((product, index) => (
               <Link
                 key={product.id}
